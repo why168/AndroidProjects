c60de95bf88db9b49cfb79f92d4483eb9c254a12
Edwin Wu <edwin_wu05@126.com>
2017-05-21 10:06:22 +0800

    kotlin

--- InterView/app/src/main/java/com/github/why168/interview/broadcast/MyReceiver1.java
+++ InterView/app/src/main/java/com/github/why168/interview/broadcast/MyReceiver1.java
@@ -1,9 +1,11 @@
 package com.github.why168.interview.broadcast;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.github.why168.interview.constant.Constants;
 
@@ -19,5 +21,22 @@ public class MyReceiver1 extends BroadcastReceiver {
         System.out.println("MyReceiver1 接受到消息, 收到: " + broadcast_data);
 
         abortBroadcast(); //中断广播，不会再响比它有优先级低得广播再传播下去了
+
+        showDialog(context);
+
+        showDialog(context);
+    }
+
+    public void showDialog(Context context) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        dialogBuilder.setTitle("提示");
+        dialogBuilder.setMessage("无可用WiFi,使用移动网络继续播放?");
+        dialogBuilder.setCancelable(false);
+        dialogBuilder.setPositiveButton("确定", null);
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        boolean showing = alertDialog.isShowing();
+        System.out.println("showing = " + showing);
+        alertDialog.show();
     }
 }
---
+++ KotlinLearning/app/src/main/kotlin/com/github/why168/kotlinlearning/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.github.why168.kotlinlearning
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        hello.text = "Hello, world!"
+    }
+}
